29f571b5b99149b19d4613cd3b2c27a1ded89627
gcamp <guillaume.campagna@gmail.com>
2014-03-22 15:27:07 -0400

    Fix CPU usage at idle

--- public/parkingmap.js
+++ public/parkingmap.js
@@ -5,7 +5,7 @@ $.parkingMap = {
 	objectIds: [],
 	objectTags: {},
 	payingParkingsVisible: true,
-	
+
 	objectFunctions: {
 		marker: {add: "addMarker", update: "updateMarker"},
 		point: {add: "addPoint", update: "updatePoint"}
@@ -16,14 +16,13 @@ $.parkingMap = {
 		this.assets = assets;
 		map.createMap(selector, center, zoom);
 		map.addOnLoadEvent(onLoad);
-		setInterval(function() {$.parkingMap.update();}, 250);
 		map.addOnChangeEvent(function() {$.parkingMap.update();});
 	},
-	
+
 	addUpdateEvent: function(update) {
 		this.map.addOnChangeEvent(update);
 	},
-	
+
 	addObject: function(object) {
 		if (this.assets.hasOwnProperty(object.type)) {
 			var objectAssets = this.assets[object.type];
@@ -43,7 +42,7 @@ $.parkingMap = {
 			}
 		}
 	},
-	
+
 	isObjectVisible: function(assets, object) {
 		var visible = true;
 		if (assets.minZoom != undefined) {
@@ -54,7 +53,7 @@ $.parkingMap = {
 		}
 		return visible;
 	},
-	
+
 	initObjectTagInformation: function(tag) {
 		if (this.objectTags[tag] == undefined) {
 			this.objectTags[tag] = {
@@ -63,57 +62,57 @@ $.parkingMap = {
 			};
 		}
 	},
-	
+
 	getObjectTagInformation: function(tag) {
 		this.initObjectTagInformation(tag);
 		return this.objectTags[tag];
 	},
-	
+
 	addObjectTagObject: function(tag, id) {
 		this.initObjectTagInformation(tag);
 		this.objectTags[tag].objects.push(id);
 	},
-	
+
 	setObjectTagVisible: function(tag, visible) {
 		this.initObjectTagInformation(tag);
 		this.objectTags[tag].visible = visible;
 	},
-	
+
 	objectExists: function(object) {
 		return (object.id != undefined && this.objects[object.id] != undefined);
 	},
-	
+
 	addMarker: function(assets, object, visible) {
 		return this.map.createMarker(object.position, visible, assets.icon, object.description);
 	},
-	
+
 	updateMarker: function(object, visible) {
 		this.map.setMarkerVisible(object.instance, visible);
 		return object.instance;
 	},
-	
+
 	addPoint: function(assets, object, visible) {
 		return this.map.createPoint(object.position, assets.size, assets.color, visible, object.description);
 	},
-	
+
 	updatePoint: function(object, visible) {
 		this.map.setPointVisible(object.instance, visible);
 		return object.instance;
 	},
-	
+
 	update: function() {
 		for (var i=0; i<this.objectIds.length; i++) {
 			var objectInfo = this.objects[this.objectIds[i]];
 			this[this.objectFunctions[objectInfo.assets.type].update](objectInfo, this.isObjectVisible(objectInfo.assets, objectInfo.object));
 		}
 	},
-	
+
 	addSearchBar: function() {
 		var input = (document.getElementById('pac-input'));
 		this.map.addTopLeftElement(input);
 		this.map.addSearch(input);
 	},
-	
+
 	addButton: function(button) {
 		this.map.addBottomLeftElement(button);
 	},
@@ -122,15 +121,15 @@ $.parkingMap = {
 		this.setObjectTagVisible(tag, visible);
 		this.update();
 	},
-	
+
 	getBounds: function() {
 		return this.map.getBounds();
 	},
-	
+
 	getZoom: function() {
 		return this.map.getZoom();
 	},
-	
+
 	addSettings: function() {
 		var settingsButton = $("<a href=\"#settings\" id=\"open-settings\">Settings</a>");
 		this.addButton(settingsButton[0]);
