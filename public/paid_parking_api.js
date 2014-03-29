7e2f35e443157d6eacb6b86df1a158a6346bc38c
hugolhml <email@hugolh.com>
2014-03-29 13:27:37 -0400

    Changement design HTML (paramètres + Infobox)

--- public/paid_parking_api.js
+++ public/paid_parking_api.js
@@ -39,7 +39,7 @@ $.API = {
 			mapObject.type = getVehiculeParcType(occupancy);
 		}
 		mapObject.available = (avl.OPER - avl.OCC);
-		mapObject.description = "<div><p>Ce point de stationnement rapporte que <b>" + (avl.OPER - avl.OCC) + "</b> stationnement(s) sont libres.</p><p>Adresse: <b style=\"cursor:pointer;\" onclick=\"$.parkingMap.getDirectionsTo({latitude: "+mapObject.position.latitude+", longitude: "+mapObject.position.longitude+"});\">" + description + "</b></p></div>";
+		mapObject.description = "<div><p>Ce point de stationnement rapporte que <b>" + (avl.OPER - avl.OCC) + "</b> stationnement(s) sont libres.</p><p>Adresse: <b>" + description + "</b></p><p><button class=\"expand\" onclick=\"$.parkingMap.getDirectionsTo({latitude: "+mapObject.position.latitude+", longitude: "+mapObject.position.longitude+"});\">Obtenir l'itinéraire</button></p></div>";
 		mapObject.label = "<div class=\""+mapObject.type+"\">"+mapObject.available+"</div>";
 		callback(mapObject);
 	}
